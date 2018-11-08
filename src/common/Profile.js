65a0d5c2e3bc3af59042c7b7eda5b5ff3234d1f8
XinyuLi <xil672@ucsd.edu>
2018-11-07 22:19:16 -0800

    fixed hoverable dropdown

--- src/common/Profile.js
+++ src/common/Profile.js
@@ -1,55 +1,63 @@
+
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 
-
-class ProfileDropdown extends React.Component {
-	constructor(props){
-		super(props);
-
-		this.state = {
-			displayMenu: false
-		}
-
-
-		this.showDropdownMenu = this.showDropdownMenu.bind(this);
-		this.hideDropdownMenu = this.hideDropdownMenu.bind(this);
-
-	};
-	showDropdownMenu(event) {
-		event.preventDefault();
-		this.setState({ displayMenu: true }, () => {
-			document.addEventListener('mouseout', this.hideDropdownMenu);
-		});
-	}
-
-	hideDropdownMenu() {
-		this.setState({ displayMenu: false }, () => {
-			document.removeEventListener('mouseover', this.hideDropdownMenu);
-		});
-
-	}
-
-	render() {
-		return (
-			<div  className="dropdown" style = {{background:"lightgrey", width:"200px"}}>
-			<div className="button" onMouseOver={this.showDropdownMenu}><a href ="#">Profile</a></div>
-
-			{ this.state.displayMenu ? (
-				<ul style = {{display:"inline-block"}}>
-				<li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></li>
-				<li><a href="#">Integer porta risus eget quam consequat, ac consequat arcu lacinia.</a></li>
-				<li><a href="#">Morbi vehicula nisi tincidunt convallis sollicitudin.</a></li>
-				</ul>
-			):
-				(
-					null
-				)
-			}
-
-			</div>
-
-		);
-	}
+class Dropdown extends React.Component {
+  render() {
+    return (
+      <ul>
+        <li>
+          <a>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a>
+        </li>
+        <li>
+          <a>Integer porta risus eget quam consequat, ac consequat arcu lacinia.</a>
+        </li>
+        <li>
+          <a>Morbi vehicula nisi tincidunt convallis sollicitudin.</a>
+        </li>
+      </ul>
+    )
+  }
 }
 
+class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showDropDown: false
+    };
+  }
+  
+  handleHover = (event) => {
+    this.setState({ showDropDown: true });
+  };
+  
+  handleLeave = (event) => {
+    this.setState({ showDropDown: false });
+  };
+  
+  render() {
+    return (
+      <ul>
+      <li onMouseLeave={this.handleLeave}>
+            <a onMouseEnter={this.handleHover} href="#">
+              Profile
+            </a>
+            <div>
+              
+                { this.state.showDropDown && <Dropdown /> }
+              
+            </div>
+          </li>
+      </ul>
 
-export default ProfileDropdown;
+    )
+  }
+}
+
+ReactDOM.render(
+  <Profile />,
+  document.getElementById("root")
+);
+
+export default Profile;
