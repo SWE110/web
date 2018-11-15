7d20e54ae7282b89e6a9297b273b99f12ac76890
ryl020 <ryl020@ucsd.edu>
2018-11-15 13:13:08 -0800

    updated ProfilePage

--- src/container/ProfilePage.js
+++ src/container/ProfilePage.js
@@ -2,9 +2,13 @@
 import React, {Component} from 'react'
 
 import { Link } from 'react-router-dom'
-import { Container, Box, Table, Title, SubTitle, Button, Notification } from 'reactbulma'
+import { Container, Box, Table, Title, SubTitle, Button, Input, Notification } from 'reactbulma'
 
 import './onboard.css'
+
+var notification = 'Information Updated'
+var name  = 'a;lskdjfa;ls'
+var email = 'asdfas'
 
 class ProfilePage extends Component {
     constructor(props) {
@@ -13,13 +17,24 @@ class ProfilePage extends Component {
         this.state = {
             notify: false
         }
-        this.handleUpdatePassword = this.handleUpdatePassword.bind(this)
+        this.handleUpdateInfo = this.handleUpdateInfo.bind(this)
     }
 
-    handleUpdatePassword() {
+    handleUpdateInfo() {
         this.setState(state => ({
             notify: !state.notify
         }))
+        if(this.state.notify){
+            var newName = document.getElementById('newName').value
+            var newPass = document.getElementById('newPass').value
+
+
+            if(newName == '' && newPass == ''){
+                notification = 'No new information entered'
+            }
+
+        }
+
         setTimeout(() => {
             this.setState(state => ({
                 notify: false
@@ -36,23 +51,28 @@ class ProfilePage extends Component {
                 <SubTitle className="has-weight-bold">Basic Information</SubTitle>
                 <table width="100%" class="table is-bordered">
                     <tr>
-                        <td width="25%" className="has-text-weight-bold has-background-grey-lighter">Name</td>
-                        <td width="75" className="has-background-white-ter">aasdf</td>
+                        <td width="20%" className="has-text-weight-bold has-background-grey-lighter">Name</td>
+                        <td width="40%" className="has-background-white-ter">
+                            {name}
+                            <input className="input" type="text" placeholder="Insert new name" id="newName"></input>
+                        </td>
                     </tr>
                     <tr>
-                        <td width="25%" className="has-text-weight-bold has-background-grey-lighter">Email</td>
-                        <td width="75" className="has-background-white-ter">aasdf</td>
+                        <td width="20%" className="has-text-weight-bold has-background-grey-lighter">Email</td>
+                        <td width="40%" className="has-background-white-ter">{email}</td>
                     </tr>
                 </table>
                 <SubTitle className="has-text-weight-bold">Password</SubTitle>
 
-                <p id="padding"> A strong password contains a mix of numbers, letters, and symbols. It is hard to guess, does not resemble a real word,
+                <p> A strong password contains a mix of numbers, letters, and symbols. It is hard to guess, does not resemble a real word,
                     and is only used for this account</p>
-
-                <Button className="is-info" onClick={this.handleUpdatePassword}>Update Password</Button>
+                <div id="padding"></div>
+                <input  className="input" type="password" placeholder="Insert new password" id="newPass"></input>
+                <div id="padding"></div>
+                <input className="button is-info" type="submit" value="Submit Changes" onClick={this.handleUpdateInfo}></input>
                 <div className="notify spacing-base">
                     {this.state.notify ?
-                        <Notification warning id="notify">Kappa</Notification>:
+                        <Notification warning id="notify">{notification}</Notification>:
                         ''}
                 </div>
             </Container>
